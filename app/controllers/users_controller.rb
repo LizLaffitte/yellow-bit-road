fb7e4360b56e0aaa8631dabccf0f5bf774e1172e
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-05 21:08:24 -0400

    Created Users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to '/login'
+        else
+            binding.pry
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password_digest, :email)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :paths
     has_many :checkpoints
     has_many :courses, through: :checkpoints
-    
+    has_secure_password
+    validates :email, uniqueness: true
+    validates :username, uniqueness: true, length: {in: 6..20}
 end
