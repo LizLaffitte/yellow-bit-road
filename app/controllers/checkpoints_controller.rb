9456dc721fe2ae00b2da145beb89576a70d701ac
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-08 16:29:24 -0400

    Created new checkpoints form. Not working at the moment.

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -1,7 +1,15 @@
 class CheckpointsController < ApplicationController
     def new
+        @road = Road.find_by(id: params[:road_id])
+        @checkpoint = @road.checkpoints.build(road_id: @road.id)
     end
 
     def create
+        @checkpoint = Checkpoint.new(checkpoint_params)
+    end
+
+    private
+    def checkpoint_params
+        params.require(:checkpoint).permit(:order, :goal_date, :completed, :course_id, :road_id, :user_id)
     end
 end
