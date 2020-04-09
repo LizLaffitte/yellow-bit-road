7f2011c143f20b4126662e65d1197201adcb20a7
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-09 17:23:46 -0400

    Added validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :checkpoints
     has_many :courses, through: :checkpoints
     has_secure_password
-    validates :email, uniqueness: true
-    validates :username, uniqueness: true, length: {in: 6..20}
+    validates :email, uniqueness: true, presence: true
+    validates :username, uniqueness: true, length: {in: 6..20}, presence: true
+    
 end
