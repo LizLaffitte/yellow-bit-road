44c1e064688f97aab4d7f7f9d28c6a4f5f4f9814
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-10 11:23:40 -0400

    Moved validation from course to checkpoint for goal_date

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -2,4 +2,13 @@ class Checkpoint < ApplicationRecord
     belongs_to :course
     belongs_to :road
     belongs_to :user
+
+    validate :goal_date_cannot_be_in_the_past
+
+    def goal_date_cannot_be_in_the_past
+      if goal_date.present? && goal_date < Date.today
+        errors.add(:goal_date, "can't be in the past")
+      end
+    end    
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -5,12 +5,6 @@ class Course < ApplicationRecord
     validates :name, presence: true, length: {in: 3...100}, uniqueness: true
     validates :description, length: {maximum: 300}
     validates :difficulty, inclusion: { in: %w(easy moderate challenging), message: "%{difficulty} is not a valid difficulty level" }
-    validate :goal_date_cannot_be_in_the_past
-
-    def goal_date_cannot_be_in_the_past
-      if goal_date.present? && goal_date < Date.today
-        errors.add(:goal_date, "can't be in the past")
-      end
-    end    
+  
 
 end
