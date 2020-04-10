6b22fe8a121c775d40458fc0182486f92d1d023e
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-10 11:32:05 -0400

    Added delete button to edit_menu partial.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
     def is_road_owner?(road)
         if road.user == current_user
-            render partial: "users/edit_menu", locals: {child_path: edit_user_road_path(road)}
+            render partial: "users/edit_menu", locals: {child_path: edit_user_road_path(road), child_delete_path: user_road_path(road)}
         end
     end
 
