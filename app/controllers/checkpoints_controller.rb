dda4eb84fe114046c85780126ba57244c55da50e
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-10 20:51:49 -0400

    creating the new checkpoints form.

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -1,7 +1,14 @@
 class CheckpointsController < ApplicationController
     def new
         if params[:road_id]
-            @road = params[:road_id]
+            @road = Road.find_by(id: params[:road_id])
+            if @road.user == current_user
+                @checkpoint = Checkpoint.new(road_id: params[:road_i])
+            else
+                redirect_to roads_path
+            end
+        else   
+            @checkpoint = Checkpoint.new
         end
 
     end
