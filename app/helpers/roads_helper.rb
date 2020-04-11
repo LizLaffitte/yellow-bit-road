c5fe8d39ca2da42e893bfd74238737f6ad2f25bd
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-11 13:38:49 -0400

    Moving logic of checkpoint fields into help method and partials.

--- app/helpers/roads_helper.rb
+++ app/helpers/roads_helper.rb
@@ -1,6 +1,10 @@
 module RoadsHelper
 
-    def checkpoints_options
-        
+    def checkpoints_options(road, f)
+        if road.new_record?
+            render partial: "new_checkpoints", locals: {f: f}
+        else
+            render partial: "existing_checkpoints", locals: {f: f}
+        end
     end
 end
