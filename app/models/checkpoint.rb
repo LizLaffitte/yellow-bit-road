d34876a1197c61c2ffa8de6646a0ccc1626674ab
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-11 11:45:50 -0400

    Fixed ability to select or create new course from new checkpoints form. Changed new road path in header nav.

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -2,7 +2,7 @@ class Checkpoint < ApplicationRecord
     belongs_to :course
     belongs_to :road
     belongs_to :user
-    accepts_nested_attributes_for :course
+    accepts_nested_attributes_for :course, reject_if: proc { |attributes| attributes['name'].blank? }
     validate :goal_date_cannot_be_in_the_past
 
     def goal_date_cannot_be_in_the_past
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :destroy] do
     resources :roads
   end
-  resources :roads do
+  resources :roads, only: [:index, :show] do
     resources :checkpoints, only: [:new, :create, :show, :destroy, :edit, :update]
   end
   
