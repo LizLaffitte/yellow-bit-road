2e78d1f6723efaaf8a555e79317b3e4a53424940
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-15 15:23:53 -0400

    Fixed error message issue for language_option. Changed capitalization of difficulty_options method.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,12 +3,12 @@ class Course < ApplicationRecord
     has_many :roads, through: :checkpoints
     has_many :users, through: :checkpoints
     validates :name, presence: true, length: {in: 3...100}, uniqueness: true
-    validates :language, presence: true, inclusion: {in: %w(CSS HTML RUBY PHP JavaScript Python), message: "%{language} is not currently an option. Email an admin to ask for your language to be included" }
+    validates :language, presence: true, inclusion: {in: %w(CSS HTML Ruby PHP JavaScript Python), message: "is not currently an option. Email an admin to ask for your language to be included" }
     validates :description, length: {maximum: 300}
-    validates :difficulty, presence: true, inclusion: { in: %w(easy moderate challenging), message: "%{difficulty} is not a valid difficulty level" }
+    validates :difficulty, presence: true, inclusion: { in: %w(Easy Moderate Challenging), message: "%{difficulty} is not a valid difficulty level" }
     
     def self.difficulty_levels
-        [['Easy', 'easy'], ['Moderate', 'moderate'], ['Challenging', 'challenging']]
+        ['Easy', 'Moderate', 'Challenging']
     end
 
     def self.language_options
