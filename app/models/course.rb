0362c575a4a422d98a3287352abf66f9c83fac7e
LizLaffitte <liz.kossakowski@gmail.com>
2020-04-18 10:55:02 -0400

    Added wrapper div to roads and checkpoint forms.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ApplicationRecord
     has_many :roads, through: :checkpoints
     has_many :users, through: :checkpoints
     validates :name, presence: true, length: {in: 3...100}, uniqueness: true
-    validates :topic, presence: true, inclusion: {in: ['CSS', 'HTML', 'Ruby', 'PHP', 'JavaScript', 'Python', 'Web Development', 'Cyber Security', 'Web Design', 'Java', 'iOS', 'Android', 'C#'], message: "is not currently an option. Email an admin to ask for your topic to be included" }
+    validates :topic, presence: true, inclusion: {in: ['CSS', 'HTML', 'Ruby', 'PHP', 'JavaScript', 'Python', 'Web Development', 'Cyber Security', 'Web Design', 'Java', 'iOS', 'Android', 'C#', 'SQL'], message: "is not currently an option. Email an admin to ask for your topic to be included" }
     validates :description, length: {maximum: 300}
     validates :difficulty, presence: true, inclusion: { in: %w(Easy Moderate Challenging), message: "is not a valid difficulty level" }
     
@@ -12,7 +12,7 @@ class Course < ApplicationRecord
     end
 
     def self.topic_options
-        ['CSS', 'HTML', 'Ruby', 'PHP', 'JavaScript', 'Python', 'Web Development', 'Cyber Security', 'Web Design', 'Java', 'iOS', 'Android', 'C#']
+        ['CSS', 'HTML', 'Ruby', 'PHP', 'JavaScript', 'Python', 'Web Development', 'Cyber Security', 'Web Design', 'Java', 'iOS', 'Android', 'C#', 'SQL'].sort
     end
 
   
