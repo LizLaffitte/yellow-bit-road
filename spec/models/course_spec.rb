06c2d1b3e324a050e45642fef63f68131a914a5c
lizlaffitte <laffitte.digital@gmail.com>
2020-11-27 17:31:44 -0500

    Incorporate course model validation tests.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 
 RSpec.describe Course, :type => :model do
-    subject {described_class.new(name: "Python Basics", description: "Learn the building blocks of the wonderful general purpose programming language Python.", difficulty: "Easy", topic:"Python")}
+    subject {described_class.new(
+                name: "Python Basics", 
+                description: "Learn the building blocks of the wonderful general purpose programming language Python.", 
+                difficulty: "Easy", 
+                topic:"Python")
+            }
     it "is valid with valid attributes" do
         expect(subject).to be_valid
     end
@@ -13,6 +18,16 @@ RSpec.describe Course, :type => :model do
 
     it "is not valid without a description" do
         subject.description = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a difficulty" do
+        subject.difficulty = nil
+        expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a valid difficulty" do
+        subject.difficulty = "Easy Enough"
         expect(subject).to_not be_valid
     end
   
